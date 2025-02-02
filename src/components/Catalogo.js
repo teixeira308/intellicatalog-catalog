ee73ba31baf0d45af944c5f062de9f69d3846b01
Guilherme Teixeira <teixeira308@hotmail.com>
2025-02-02 14:02:04 -0300

    fix

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -173,7 +173,7 @@ function Catalogo() {
 
   const fetchCategories = async (storeId) => {
     try {
-      const response = await axios.get(`${api_url}/intellicatalog/v1/categories/users/${storeId}?mode=store`, {
+      const response = await axios.get(`${api_url}/intellicatalog/v1/categories/users/${storeId}`, {
         headers: {
           Authorization: `Bearer ${apiToken}`
         }
