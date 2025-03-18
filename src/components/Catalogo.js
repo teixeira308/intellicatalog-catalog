b9d497fe94e4943f4f52199937b7fd20ee66829b
Guilherme Teixeira <teixeira308@hotmail.com>
2025-03-18 14:31:02 -0300

    fix stageloading

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -367,7 +367,7 @@ function Catalogo() {
           })
         );
         newImages[product.id] = fotosUrls.filter(Boolean); // Adiciona as URLs válidas
-        setLoadingStage(3);
+        
         console.log(`Loaded images for product ${product.id}:`, fotosUrls.filter(Boolean));
       })
     );
@@ -378,7 +378,7 @@ function Catalogo() {
         ...newImages, // Mescla as novas imagens com as existentes
       };
       console.log("Updated product images:", updatedImages);
-      setLoadingStage(2);
+      setLoadingStage(3);
       return updatedImages;
     });
   };
@@ -399,6 +399,7 @@ function Catalogo() {
       loadStoreConfigs(storeDetails);
       loadStoreImages(storeDetails);
       fetchCategories(storeDetails.user_id);
+      setLoadingStage(2);
     }
   }, [storeDetails]);
 
